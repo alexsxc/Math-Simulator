0efa7a3008aed6b0c3399915642f4b9b59af85ab
Ramitsan <ramitsan@gmail.com>
2024-05-11 20:51:36 +0300

    add dependencies

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,21 @@ import './style.css';
 export default function App() {
   return (
     <>
-      <div className="wrapper"></div>
+    <header>
+      <div className="logo">
+        <img src="" alt="" />
+        <p>Математический тренажер</p>
+      </div>
+    </header>
+    <div className="wrapper">
+      <div className="status-bar">
+        <div className="level-path">Тренажер / Арифметические действия с обыкновенными дробями. Умножение.</div>
+        <div className="level-progress"></div>
+        <div className="step"></div>
+        <button className="next-level"></button>
+      </div>
+      <div className="game-field"></div>
+    </div>
     </>
   )
 }
